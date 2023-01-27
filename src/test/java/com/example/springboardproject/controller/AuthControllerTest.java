477d43edfe86b16aba0f78c01f246f7e1f569e77
Kimhyeyun <cqqwer@gmail.com>
2023-01-27 16:00:34 +0900

    리팩토링:인증 테스트 실패로 리팩토링
    테스트 코드내에 securityConfig 파일 import가 잘못되어 있어서 수정

--- src/test/java/com/example/springboardproject/controller/AuthControllerTest.java
+++ src/test/java/com/example/springboardproject/controller/AuthControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.springboardproject.controller;
 
 import com.example.springboardproject.config.SecurityConfig;
+import com.example.springboardproject.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Void.class)
 public class AuthControllerTest {
 
