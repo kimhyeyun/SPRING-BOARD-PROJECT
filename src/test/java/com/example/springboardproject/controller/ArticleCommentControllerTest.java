96fd27f7651ca2f3fbe0a11219d5bf4106dad746
HYEYUN <50236501+kimhyeyun@users.noreply.github.com>
2023-01-31 20:56:53 +0900

    Revert "#101 - 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가"
    This reverts commit 90004d497e4c19c62644506d4d5e6f825e86cd68.

--- src/test/java/com/example/springboardproject/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/example/springboardproject/controller/ArticleCommentControllerTest.java
@@ -56,10 +56,10 @@ class ArticleCommentControllerTest {
 
         // When & Then
         mvc.perform(
-                post("/comments/new")
-                        .content(formDataEncoder.encode(request))
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .with(csrf())
+                        post("/comments/new")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .content(formDataEncoder.encode(request))
+                                .with(csrf())
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/articles/" + articleId))
@@ -81,38 +81,16 @@ class ArticleCommentControllerTest {
 
         // When & Then
         mvc.perform(
-                post("/comments/" + articleCommentId + "/delete")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .content(formDataEncoder.encode(Map.of("articleId", articleId)))
-                        .with(csrf())
+                        post("/comments/" + articleCommentId + "/delete")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .content(formDataEncoder.encode(Map.of("articleId", articleId)))
+                                .with(csrf())
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/articles/" + articleId))
                 .andExpect(redirectedUrl("/articles/" + articleId));
 
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
-    }
 
-    @WithUserDetails(value = "yunTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    @DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
-    @Test
-    void givenArticleCommentInfoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
-        // Given
-        long articleId = 1L;
-        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
-        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
-
-        // When & Then
-        mvc.perform(
-                post("/comments/new")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .content(formDataEncoder.encode(request))
-                        .with(csrf())
-                )
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/articles/" + articleId))
-                .andExpect(redirectedUrl("/articles/" + articleId));
-
-        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
     }
 }
