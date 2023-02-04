7848df0cda8330c8fc37ef2d4d65fb77ed72b49e
Kimhyeyun <cqqwer@gmail.com>
2023-02-04 16:57:19 +0900

    #84 - Swagger UI 테스트

--- src/main/java/com/example/springboardproject/controller/MainController.java
+++ src/main/java/com/example/springboardproject/controller/MainController.java
@@ -1,7 +1,17 @@
 package com.example.springboardproject.controller;
 
+import com.example.springboardproject.dto.response.ArticleCommentResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 메인 컨트롤러
+ * </p>
+ */
 
 @Controller
 public class MainController {
@@ -9,5 +19,25 @@ public class MainController {
     @GetMapping("/")
     public String root() {
         return "forward:/articles";
+    }
+
+    /**
+    * 댓글 정보를 열람한다.
+    *
+    * @param id 댓글 ID
+    * @return 댓글 응답
+    *
+    * */
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(
+                id,
+                "content",
+                LocalDateTime.now(),
+                "t@mail.com",
+                "test",
+                "test"
+        );
     }
 }
